61bc0bb3d2c1cd77abbfb8dfd9456049761b9fa5
rberlind <roger@hashicorp.com>
2019-10-07 11:48:35 -0400

    simpler

--- main.tf
+++ main.tf
@@ -12,16 +12,6 @@ provider "aws" {
   alias = "root-west"
 }
 
-resource "aws_instance" "web" {
-  provider = "aws.root-east"
-  ami           = "ami-2e1ef954"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "root module instance"
-  }
-}
-
 module "first" {
   source = "./module"
 }
--- module/main.tf
+++ module/main.tf
@@ -11,17 +11,3 @@ provider "aws" {
   region     = "us-west-1"
   alias = "first-west"
 }
-
-resource "aws_instance" "web" {
-  provider = "aws.first-east"
-  ami           = "ami-2e1ef954"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "first module instance"
-  }
-}
-
-module "second" {
-  source = "./module"
-}
